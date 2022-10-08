dc101242764330ed6fb1b487397ce2aed2b328ba
Lucas Mateus <lmateus1067@outlook.com>
2022-10-08 10:49:59 -0300

    nao funciona

--- Projetos pessoais/validar_nome.c
+++ Projetos pessoais/validar_nome.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <string.h>
-
+int validar_nome(char nome[]);
 
 int main(void)
 {
 
+    char nome[52];
+
+    printf("Insira um nome\n");
+    scanf("%[A-Z a-z]", nome);
+    getchar();
+
+    while (!validar_nome(nome))
+    {
+        printf("Nome invalido!\n");
+        scanf("%[A-Z a-z]", nome);
+        getchar();
+    }
+
     return 0;
+}
+
+int validar_nome(char nome[])
+{
+
+    for (int i; nome[i] != '\0'; i++)
+    {
+
+        if (((nome[i] > "z") || (nome[i] < "a")) && ((nome[i] > "Z") || (nome[i] < "A")))
+        {
+
+            return 0;
+        }
+        else
+        {
+
+            return 1;
+        }
+    }
 }
