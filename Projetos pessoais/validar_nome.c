fae8e5294c63f884cb16a435e96fc2c5476ea188
Lucas Mateus <lmateus1067@outlook.com>
2022-10-10 13:43:00 -0300

    valida

--- Projetos pessoais/validar_nome.c
+++ Projetos pessoais/validar_nome.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-int validar_nome(char nome[]);
+int validar_letras(char nome[]);
 
 int main(void)
 {
@@ -8,10 +8,10 @@ int main(void)
     char nome[52];
 
     printf("Insira um nome\n");
-    scanf("%[A-Z a-z]", nome);
+    scanf("%s", nome);
     getchar();
 
-    while (!validar_nome(nome))
+    while (!(validar_letras(nome)))
     {
         printf("Nome invalido!\n");
         scanf("%[A-Z a-z]", nome);
@@ -21,21 +21,27 @@ int main(void)
     return 0;
 }
 
-int validar_nome(char nome[])
+int validar_letras(char nome[])
 {
 
-    for (int i; nome[i] != '\0'; i++)
+    for (int i = 0; nome[i] != '\0'; i++)
     {
 
-        if (((nome[i] > "z") || (nome[i] < "a")) && ((nome[i] > "Z") || (nome[i] < "A")))
+        if (((nome[i] <= 'z') && (nome[i] >= 'A')))
         {
 
-            return 0;
+            if (((nome[i] > 'Z') && (nome[i] < 'a')))
+            {
+
+                return 0;
+            }
         }
         else
         {
 
-            return 1;
+            return 0;
         }
     }
+
+    return 1;
 }
